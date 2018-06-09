c948b700e0ea0df8beac70580d7b237686a35745
Marc Deveaux <mdvx@users.noreply.github.com>
2018-06-08 21:09:58 -0400

    re-org

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 echo Restoring NuGet packages
-nuget\nuget.exe restore src\rabbit-rtd.sln
+nuget\nuget.exe restore rabbit-rtd.sln
 
 echo Building release
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /p:Configuration=Release /v:minimal /nologo src\rabbit-rtd.sln
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /p:Configuration=Release /v:minimal /nologo rabbit-rtd.sln
--- register.debug.cmd
+++ register.debug.cmd
@@ -1,4 +1,4 @@
 @echo off
 
 echo Registering RTD Server
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe src\RabbitRtd\bin\Debug\RabbitRTD.dll /codebase
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe RabbitRtd\bin\Debug\RabbitRTD.dll /codebase
