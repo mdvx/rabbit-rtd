f39f69829d9b0c0bd7eae572b37391a25487c95b
Marc Deveaux <mdvx@users.noreply.github.com>
2018-06-14 20:34:20 -0400

    fix part of the build script

--- build.cmd
+++ build.cmd
@@ -1,7 +1,13 @@
 @echo off
 
+echo download nuget from https://www.nuget.org/downloads
 echo Restoring NuGet packages
-nuget\nuget.exe restore rabbit-rtd.sln
+
+nuget restore rabbit-rtd.sln
+
+@if ERRORLEVEL 1 PAUSE
 
 echo Building release
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /p:Configuration=Release /v:minimal /nologo rabbit-rtd.sln
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /p:Configuration=Release /v:minimal  .\rabbit-rtd.sln
+
+@if ERRORLEVEL 1 PAUSE
